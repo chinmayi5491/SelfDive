9b83c8512521043329067c725ab76d6cab3086c3
Stephen Weber <stephen.weber3@gmail.com>
2018-04-09 20:27:02 -0400

    api implementation work

---
+++ app/src/main/java/com/app/selfdive/DisplaySize.java
@@ -0,0 +1,18 @@
+package com.app.selfdive;
+
+/**
+ * Created by Stephen on 4/9/2018.
+ */
+
+public class DisplaySize {
+
+    private String uri;
+
+    public String getUri() {
+        return uri;
+    }
+
+    public void setUri(String uri) {
+        this.uri = uri;
+    }
+}
---
+++ app/src/main/java/com/app/selfdive/ImageResult.java
@@ -0,0 +1,19 @@
+package com.app.selfdive;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Stephen on 4/9/2018.
+ */
+
+public class ImageResult {
+    private ArrayList<Image> images;
+
+    public ArrayList<Image> getImages() {
+        return images;
+    }
+
+    public void setImages(ArrayList<Image> images) {
+        this.images = images;
+    }
+}
